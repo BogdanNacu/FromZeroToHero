fb3371171d0eef74a7f93ad04d5df89818f72acb
BogdanNacu <bogdan.a.nacu@gmail.com>
2015-05-12 17:07:15 +0300

    Added PrintSeason function.

--- TestExercitii/Program.cs
+++ TestExercitii/Program.cs
@@ -8,26 +8,60 @@ namespace TestExercitii
 {
     class Program
     {
+        enum Season
+        { 
+            Spring,
+            Summer,
+            Autumn,
+            Winter
+        }
+
         static void Main(string[] args)
         {
+            /****************
             bool isWeekend = true;
-            DateTime azi = new DateTime();
+            DateTime azi = DateTime.Now;
             
             isWeekend = IsWeekendDay(azi);
-            Console.WriteLine(isWeekend);
+            Console.WriteLine("Is " + azi.ToString() + " a weekend day? " + isWeekend);
 
             DateTime date = new DateTime(2015, 5, 10);
             isWeekend = IsWeekendDay(date);
-            Console.WriteLine(isWeekend);
+            Console.WriteLine("Is " + date.ToString() + " a weekend day? " + isWeekend);
 
             DateTime date2 = new DateTime(2015, 5, 2);
             isWeekend = IsWeekendDay(date2);
-            Console.WriteLine(isWeekend);
+            Console.WriteLine("Is " + date2.ToString() + " a weekend day? " + isWeekend);
+             ***************/
+
+            Season s = Season.Winter;
+            PrintSeason(s);
         }
+
 
         static bool IsWeekendDay(DateTime date)
         {
             return ((date.DayOfWeek == DayOfWeek.Sunday) || (date.DayOfWeek == DayOfWeek.Saturday));
+        }
+
+
+        static void PrintSeason(Season aSeason)
+        {
+            switch (aSeason)
+            {
+                case Season.Spring:
+                    Console.WriteLine("Spring is here!");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("Summer time!");
+                    break;
+                case Season.Autumn:
+                    Console.WriteLine("It's autumn!");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("Winter is coming!");
+                    break;
+            }
         }
     }
 }
