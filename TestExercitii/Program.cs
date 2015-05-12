23a81f5601895115c843adce286ecfaddb132c58
BogdanNacu <bogdan.a.nacu@gmail.com>
2015-05-12 17:39:36 +0300

    Added LongestCountryName and GetPriceWithDiscount

--- TestExercitii/Program.cs
+++ TestExercitii/Program.cs
@@ -19,6 +19,7 @@ namespace TestExercitii
         static void Main(string[] args)
         {
             /****************
+             * Exercise 1
             bool isWeekend = true;
             DateTime azi = DateTime.Now;
             
@@ -32,10 +33,26 @@ namespace TestExercitii
             DateTime date2 = new DateTime(2015, 5, 2);
             isWeekend = IsWeekendDay(date2);
             Console.WriteLine("Is " + date2.ToString() + " a weekend day? " + isWeekend);
-             ***************/
+            
+             * Exercise 2
 
             Season s = Season.Winter;
             PrintSeason(s);
+
+             * Exercise 3+4
+            
+            string aCountry = LongestCountryName("Bulgaria11111", "Italy", "Israel", "Romania", "Bulgaria2");
+            Console.WriteLine("The country with the longest name is {0}, and it has {1} letters.", aCountry, aCountry.Length);
+
+            string[] countryArray = { "Italy", "Cuba" };
+            aCountry = LongestCountryName(countryArray);
+            Console.WriteLine("The country with the longest name is {0}, and it has {1} letters.", aCountry, aCountry.Length);
+             * 
+            ****************/
+
+            double finalPrice = 0;
+            GetPriceWithDiscount(200.10, 5, out finalPrice);
+            Console.WriteLine("The final price is: " + finalPrice);
         }
 
 
@@ -62,6 +79,43 @@ namespace TestExercitii
                     Console.WriteLine("Winter is coming!");
                     break;
             }
+        }
+
+
+        static string LongestCountryName(params string[] names)
+        {
+            string maxCountry = names[0];
+            int i = 0;
+            for (i = 1; i < names.Length - 1; i++)
+            {
+                if (names[i].Length > maxCountry.Length)
+                {
+                    maxCountry = names[i];
+                }
+            }
+
+            return maxCountry;
+        }
+
+
+        static void GetPriceWithDiscount(double price, int age, out double priceWithDiscount)
+        {
+            double discount = 0;
+
+            if (age < 7)
+            {
+                discount = 0.25;
+            }
+            else if (age < 14)
+            {
+                discount = 0.15;
+            }
+            else 
+            {
+                discount = 0.5;
+            }
+
+            priceWithDiscount = discount * price;
         }
     }
 }
