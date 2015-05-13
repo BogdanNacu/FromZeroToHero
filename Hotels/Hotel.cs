853e02e2e9f3b78529997091f871ebacde440c74
BogdanNacu <bogdan.a.nacu@gmail.com>
2015-05-13 12:07:18 +0300

    Added distanceMeasurementUnit static field to Hotel and static constructor

--- Hotels/Hotel.cs
+++ Hotels/Hotel.cs
@@ -13,6 +13,12 @@ namespace Hotels
         public double distanceToCenter;
         public DateTime openingDate;
         public Room[] rooms;
+        static public string distanceMeasurementUnit;
+
+        static Hotel()
+        {
+            distanceMeasurementUnit = "Miles";
+        }
 
         public Hotel(string name, string description, string address, int stars, double distanceToCenter, DateTime openingDate, Room[] rooms)
         {
@@ -28,12 +34,15 @@ namespace Hotels
         public void DisplayInfo()
         {
             //---Alternative to hotel display info:
-            //Console.Write("Hotel {0} at {1} opened {2} and has {3} rooms.", hotel.name, hotel.address, hotel.openingDate, hotel.rooms.Length);
-            //Console.WriteLine(" Hotel description: " + hotel.description);
+            //Console.Write("Hotel {0} at {1} opened {2} and has {3} rooms.", name, address, openingDate, rooms.Length);
+            //Console.Write("It is rated {0} stars");
+            //Console.WriteLine(" Hotel description: " + description);
             //---
 
             Console.Write("Hotel Name: {0}\n Description: {1}\n Address: {2}\n Stars: {3}\n", name, description, address, stars);
-            Console.WriteLine(" Distance to center: {0}\n Opening date: {1}\n\n Rooms: ", distanceToCenter, openingDate);
+            Console.WriteLine(" Distance to center: {0} {1}", distanceToCenter, distanceMeasurementUnit);
+
+            Console.WriteLine(" Opening date: {1}\n\n Rooms: ", distanceToCenter, openingDate);
             for (int i = 0; i < rooms.Length; i++ )
             {
                 Console.WriteLine("Room {0}:", i);
