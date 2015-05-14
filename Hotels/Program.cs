7765cd64811779c7f5f5bc71327b6ba4085a3fd3
BogdanNacu <bogdan.a.nacu@gmail.com>
2015-05-14 13:22:51 +0300

    Renamed Hotel to Property, created new Hotel and GuestHouse classes derived from Property

--- Hotels/Program.cs
+++ Hotels/Program.cs
@@ -12,18 +12,18 @@ namespace Hotels
         {
             Room room1 = new Room("This is room1.", 3, 2, RoomTypes.Single);
             Room room2 = new Room("This is room2.", 10, 5, RoomTypes.KingBedroom);
-            Hotel hotel = new Hotel("Freedom", "Pretty sweet.", "Bld. Independentei, nr. 3", 5, 12.3, new DateTime(1990, 12, 1), new Room[] { room1, room2 });
-            Hotel hotel2 = new Hotel("SpaceX", "Space themed hotel", "Moon 13", 10, 200, new DateTime(2015, 12, 1), new Room[] { room2 });
+            Property hotel = new Property("Freedom", "Pretty sweet.", "Bld. Independentei, nr. 3", 5, 12.3, new DateTime(1990, 12, 1), new Room[] { room1, room2 });
+            Property hotel2 = new Property("SpaceX", "Space themed hotel", "Moon 13", 10, 200, new DateTime(2015, 12, 1), new Room[] { room2 });
 
-            Hotel.SetDistanceMeasurementUnit("Km");
-            Console.WriteLine(Hotel.GetDistanceMeasurementUnit());
+            Property.SetDistanceMeasurementUnit("Km");
+            Console.WriteLine(Property.GetDistanceMeasurementUnit());
             hotel.DisplayInfo();
             hotel2.DisplayInfo();
 
-            Console.WriteLine("Distance to center: {0} {1}", hotel.DistanceToCenter, Hotel.GetDistanceMeasurementUnit());
+            Console.WriteLine("Distance to center: {0} {1}", hotel.DistanceToCenter, Property.GetDistanceMeasurementUnit());
             Console.WriteLine("Distance in miles: {0} Miles\n", hotel.GetDistanceToCenterInUnits("Miles"));
 
-            Hotel hotel3 = new Hotel { Name = "Wind", Description = "Feel thye winds.", Address = "Str. Fortuna, nr.2", 
+            Property hotel3 = new Property { Name = "Wind", Description = "Feel thye winds.", Address = "Str. Fortuna, nr.2", 
                 HasFreeWiFi = true, HasIndoorPool = true, DistanceToCenter = 55.3, OpeningDate = new DateTime(2010,12,5), 
                 Stars = 2, Rooms = new Room[] {room1, room2} };
             hotel3.DisplayInfo();
---
+++ Hotels/Property.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotels
+{
+    class Property
+    {
+        private string name, description, address;
+        private int stars;
+        private double distanceToCenter;
+        private DateTime openingDate;
+        private Room[] rooms;
+        static protected string distanceMeasurementUnit;
+
+        #region Properties
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Length > 50)
+                {
+                    Console.WriteLine("Max name length is 50 characters!");
+                    name = null;
+                }
+                else
+                    name = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (value.Length > 500)
+                {
+                    Console.WriteLine("Max description length is 500 characters!");
+                    description = null;
+                }
+                else
+                    description = value;
+            }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                if (value.Length > 100)
+                {
+                    Console.WriteLine("Max address length is 100 characters!");
+                    address = null;
+                }
+                else
+                    address = value;
+            }
+        }
+
+        public int Stars
+        {
+            get { return stars; }
+            set
+            {
+                if (value < 0 || value > 5)
+                {
+                    Console.WriteLine("Stars: only from 0 to 5!");
+                    stars = 0;
+                }
+                else
+                    stars = value;
+            }
+        }
+
+        public double DistanceToCenter
+        {
+            get { return distanceToCenter; }
+            set
+            {
+                if (value > 100 || value < 0)
+                {
+                    Console.WriteLine("Distance to center is between 0 to 100!");
+                    distanceToCenter = 0;
+                }
+                else
+                    distanceToCenter = value;
+            }
+        }
+
+        public DateTime OpeningDate
+        {
+            get { return openingDate; }
+            set
+            {
+                if (value > DateTime.Now || value < new DateTime(1800, 1, 1))
+                {
+                    Console.WriteLine("Max name length is 50 characters!");
+                    openingDate = new DateTime();
+                }
+                else
+                    openingDate = value;
+            }
+        }
+
+        public Room[] Rooms
+        {
+            get { return rooms; }
+            set
+            {
+                rooms = value;
+            }
+        }
+
+        public bool HasIndoorPool { get; set; }
+        public bool HasFreeWiFi { get; set; }
+        #endregion
+
+        static Property()
+        {
+            distanceMeasurementUnit = "Miles";
+        }
+
+        public Property()
+        {
+
+        }
+
+        public Property(string name, string description, string address, int stars, double distanceToCenter, DateTime openingDate, Room[] rooms)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.Address = address;
+            this.Stars = stars;
+            this.distanceToCenter = distanceToCenter;
+            this.OpeningDate = openingDate;
+            this.Rooms = rooms;
+        }
+
+        public static void SetDistanceMeasurementUnit(string measurementUnit)
+        {
+            distanceMeasurementUnit = measurementUnit;
+        }
+
+        public static string GetDistanceMeasurementUnit()
+        {
+            return distanceMeasurementUnit;
+        }
+
+        public double GetDistanceToCenterInUnits(string measurementUnit)
+        {
+            if (distanceMeasurementUnit.Equals(measurementUnit))
+                return distanceToCenter;
+            else
+                if (measurementUnit.Equals("Km"))
+                    return DistanceMeasurementConverter.TransformMilesToKilometers(distanceToCenter);
+                else
+                    if (measurementUnit.Equals("Miles"))
+                        return DistanceMeasurementConverter.TransformKilometersToMiles(distanceToCenter);
+            return -1;
+        }
+
+        public void DisplayInfo()
+        {
+            //---Alternative to hotel display info:
+            //Console.Write("Hotel {0} at {1} opened {2} and has {3} rooms.", name, address, openingDate, rooms.Length);
+            //Console.Write("It is rated {0} stars");
+            //Console.WriteLine(" Hotel description: ..." + description);
+            //---
+
+            Console.Write("Hotel Name: {0}\n Description: {1}\n Address: {2}\n Stars: {3}\n", Name, Description, Address, Stars);
+            Console.WriteLine(" Distance to center: {0} {1}", DistanceToCenter, distanceMeasurementUnit);
+
+            Console.WriteLine(" Opening date: {1}\n\n Rooms: ", DistanceToCenter, OpeningDate);
+            for (int i = 0; i < rooms.Length; i++ )
+            {
+                Console.WriteLine("Room {0}:", i);
+                rooms[i].DisplayInfo();
+            }
+        }
+    }
+}
